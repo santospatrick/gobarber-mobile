5f2d182a3052aa12a564f6ae39512a0d428ba7be
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-16 20:46:05 -0300

    adiciona botao e input

---
+++ src/components/Button/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
+import { Container, Text } from './styles';
+
+const Button = ({ children, loading, ...rest }) => {
+    return (
+        <Container {...rest}>
+            {loading ? (
+                <ActivityIndicator size="small" color="#fff" />
+            ) : (
+                <Text>{children}</Text>
+            )}
+        </Container>
+    );
+};
+
+Button.propTypes = {
+    children: PropTypes.string.isRequired,
+    loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+    loading: false,
+};
+
+export default Button;
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import Background from 'components/Background';
+import Input from 'components/Input';
+import Button from 'components/Button';
 
 const SignIn = () => {
     return (
         <Background>
             <Text>signin</Text>
+            <Input
+                style={{ marginTop: 30 }}
+                icon="call"
+                placeholder="Digite seu nome"
+            />
+            <Button>Entrar</Button>
         </Background>
     );
 };
