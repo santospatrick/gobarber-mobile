c27011258d4a99bb42b61b3de55f051eceeb7092
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-16 19:13:09 -0300

    altera estrutura inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <View
+            style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
+        >
+            <Text>Gobarber</Text>
+        </View>
+    );
+};
+
+export default App;
