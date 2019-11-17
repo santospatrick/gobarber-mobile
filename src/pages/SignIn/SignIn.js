f5983e7a1b292af76923a7f6084b6e06bd5583a3
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-16 22:18:03 -0300

    adiciona estilos no login

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from 'assets/logo.png';
+
 import Background from 'components/Background';
-import Input from 'components/Input';
-import Button from 'components/Button';
+
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 const SignIn = () => {
     return (
         <Background>
-            <Text>signin</Text>
-            <Input
-                style={{ marginTop: 30 }}
-                icon="call"
-                placeholder="Digite seu nome"
-            />
-            <Button>Entrar</Button>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua senha secreta"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => {}}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 };
