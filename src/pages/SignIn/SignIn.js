4e422422a5f9a292bd087f5f1d6fff8e65c324a3
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-16 23:04:54 -0300

    adiciona chamadas para login e cadastro

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from 'assets/logo.png';
 
@@ -22,6 +22,8 @@ const SignIn = ({ navigation }) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const loading = useSelector(state => state.auth.loading);
 
     function handleSubmit() {
         dispatch(signInRequest({ email, password }));
@@ -56,7 +58,9 @@ const SignIn = ({ navigation }) => {
                         value={password}
                     />
 
-                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>
+                        Acessar
+                    </SubmitButton>
                 </Form>
 
                 <SignLink onPress={() => navigation.navigate('SignUp')}>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from 'assets/logo.png';
 
 import Background from 'components/Background';
 
+import { signUpRequest } from 'store/modules/auth/actions';
 import {
     Container,
     Form,
@@ -15,11 +17,20 @@ import {
 } from './styles';
 
 const SignUp = ({ navigation }) => {
+    const dispatch = useDispatch();
+
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loading = useSelector(state => state.auth.loading);
+
+
     function handleSubmit() {
-        alert('submit');
+        dispatch(signUpRequest(name, email, password));
     }
 
     return (
@@ -35,6 +46,8 @@ const SignUp = ({ navigation }) => {
                         placeholder="Nome completo"
                         onSubmitEditing={() => emailRef.current.focus()}
                         returnKeyType="next"
+                        onChangeText={setName}
+                        value={name}
                     />
 
                     <FormInput
@@ -46,6 +59,8 @@ const SignUp = ({ navigation }) => {
                         ref={emailRef}
                         returnKeyType="next"
                         onSubmitEditing={() => passwordRef.current.focus()}
+                        onChangeText={setEmail}
+                        value={email}
                     />
 
                     <FormInput
@@ -55,9 +70,13 @@ const SignUp = ({ navigation }) => {
                         ref={passwordRef}
                         returnKeyType="send"
                         onSubmitEditing={handleSubmit}
+                        onChangeText={setPassword}
+                        value={password}
                     />
 
-                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>
+                        Criar conta
+                    </SubmitButton>
                 </Form>
 
                 <SignLink onPress={() => navigation.navigate('SignIn')}>
